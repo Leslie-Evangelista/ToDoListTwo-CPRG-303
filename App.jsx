4be04cf67e4c39cac7b14bb302673e39523b653e
Leslie-Evangelista <leashiaevangelista@yahoo.com>
2023-11-12 07:51:24 -0700

    Lab 3
    Lab 3

---
+++ App.jsx
@@ -0,0 +1,42 @@
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+
+import React/*, { useState } */from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Pressable,
+  View,
+  Text,
+  ScrollView,
+  TextInput,
+  Button
+} from 'react-native';
+import ToDoForm from './src/ToDoForm';
+import ToDoList from './src/ToDoList';
+
+
+function App() {
+  const {tasks, setTasks} = React.useState([
+    'Do laundry',
+    'Go to gym',
+    'Walk dog',
+    'Study'
+  ]);
+
+  return (
+    <SafeAreaView>
+      <ToDoList tasks={tasks}/>
+      <ToDoForm />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
