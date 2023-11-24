88422548385d75a7b1f52edbfeb35781e7e96c05
Leslie-Evangelista <leashiaevangelista@yahoo.com>
2023-11-24 07:46:29 -0700

    Lab 5

---
+++ src/screens/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import MainLayout from '../layouts/MainLayout';
+
+function About() {
+    return (
+        <>
+
+            <View>
+                <Text>This is Leslie's To-Do-List App</Text>
+                <Text>Created by: Leslie </Text>
+                <Text>Version 1.1.1</Text>
+            </View>
+
+
+        </>
+    );
+}
+
+export default About;
